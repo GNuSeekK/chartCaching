c65b18793b921a841b408542e1efd53f37d7c934
Keesung <person813@gmail.com>
2023-11-23 19:16:26 +0900

    initial commit

---
+++ src/main/java/stock/caching/CachingApplication.java
@@ -0,0 +1,13 @@
+package stock.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stock/caching/CachingApplicationTests.java
@@ -0,0 +1,13 @@
+package stock.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
